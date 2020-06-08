8f8339b749a6e68f237b998f16a9aba7caefce13
Raul Torres <rtorresj@unal.edu.co>
2020-06-07 22:59:34 -0500

    10:59

--- econometria 2 - 2.R
+++ econometria 2 - 2.R
@@ -96,13 +96,10 @@ linearHypothesis(Etapa1.2,c("z1=0","z2=0","z3=0")) # f= 	682.0047
 
 #3. Regresion por VI asumiendo x2 exogena ####
 
-# modelo apropiado
 VI_1=ivreg(y~x1+x2|x2+z1+z2+z3+z5, data = Datos_instrum);summary(VI_1,diagnostics = TRUE)
 
-#sobre identificado
 VI_2=ivreg(y~x1+x2|x2+z1+z2+z3, data = Datos_instrum);summary(VI_2,diagnostics = TRUE)
 
-# Modelo apropiado
 VI_3=ivreg(y~x1+x2|x2+z1+z2+z5, data = Datos_instrum);summary(VI_3,diagnostics = TRUE)
 
 stargazer(VI_1,VI_2,VI_3)
