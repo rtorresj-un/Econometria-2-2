b2d5839f440f41d68e374bf992d35b37ae2de049
Raul Torres <rtorresj@unal.edu.co>
2020-06-16 01:59:53 -0500

    2:00

--- taller 2.R
+++ taller 2.R
@@ -31,6 +31,67 @@ boxplot(ipc~ciclo)
 descompos = stl(ipc,s.window = "periodic")
 plot(descompos)
 
+par(mfrow=c(1,2))
 acf(ipc)
-pacf()
-Box.test(y, lag = 13, type = c ("Ljung-Box"))
+pacf(ipc)
+Box.test(ipc, lag = 10, type = c ("Ljung-Box")) #Ho: No correlación serial
+
+plot(diff(ipc))
+
+#Prueba Dickey-Fuller
+adf.test(diff(ipc, 2),alternative = "stationary")
+
+ru_1 <- ur.df(diff(ipc, 2), type = "drift", selectlags = c("AIC"))
+summary(ru_1)
+
+#Prueba Phillip Perron
+pptest<-ur.pp(ipc,model=c("trend"), type=c("Z-tau"))
+summary(pptest)
+
+pptest2<-ur.pp(diff(ipc),model=c("constant"), type=c("Z-tau"))
+summary(pptest2)
+
+#La prueba Kpss tiene las hip?tesis al reves
+kpss<-ur.kpss(ipc, type=c("tau"))
+summary(kpss)
+
+kpss2<-ur.kpss(diff(ipc), type=c("tau"))
+summary(kpss2)
+
+par(mfrow=c(1,2))
+acf(diff(ipc))
+pacf(diff(ipc))
+
+#ELECCI?N DEL MEJOR MODELO (SEG?N AIC) el menor indce es el mejor modelo
+
+dipc=diff(ipc, 2)
+
+arima(x = dipc,order = c(1,0,0))
+
+arima(x = dipc,order = c(2,0,4))
+
+
+# Estimaci?n de modelos
+
+mar <- 4
+mma <- 4
+results <- c("p","q","AIC","SBC")
+for (i in 0:mar) {
+  for (j in 0:mma)  {
+    fitp <- arima(dipc, order = c(i, 0, j), include.mean = TRUE)
+    results <- rbind(results,as.numeric(c(i, j, AIC(fitp), BIC(fitp)))) 
+  }
+}
+
+results
+
+ar_ipc<- arima(x = dipc,order = c(4,0,3))
+coeftest(ar_ipc)
+
+plot(ar_ipc$resid)
+
+for (i in 1:10) {print( Box.test(resid(ar_ipc), lag=i,  type="Ljung") )}
+
+hist(ar_ipc$residuals)
+tsdiag(ar_ipc)
+normalTest(ar_ipc$resid, method="jb")
