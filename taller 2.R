0dd000e11cdb1cb0d11fc1702e4d9bd762f08f3b
JuanitaCortes <jucortesar@unal.edu.co>
2020-06-15 23:37:15 -0500

    11:37

--- taller 2.R
+++ taller 2.R
@@ -10,6 +10,13 @@ library(fBasics)
 
 IPC
 ipc=ts(IPC$...1,start = c(2008,1),frequency = 12)
+class(ipc)  #Ver el tipo de objeto
+start(ipc)  #Ver cu?ndo comienza la serie
+end(ipc)  #Ver cu?ndo finaliza la serie
+frequency(ipc)  #Ver la periodicidad de los datos
+summary(ipc)  #Estad?stica descriptiva
+View(ipc)  #Ver la serie
+cs
 ipc
 acf()
 pacf()
