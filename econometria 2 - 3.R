9404f7e80679e884d091eec000980a7534db352e
Raul Torres <rtorresj@unal.edu.co>
2020-06-28 01:15:59 -0500

    1:15

--- econometria 2 - 3.R
+++ econometria 2 - 3.R
@@ -3,7 +3,16 @@
 #Primer punto####
 
 #Segundo punto####
+library(readr); library(urca); library(tseries)
+Data_UR<-read.csv(file.choose())
+View(Data_UR)
+attach(Data_UR)
+plot(x1, type = 'l')
+plot(x2, type = 'l')
+plot(x3, type = 'l')
 
 #Tercer punto####
+Data_coin<-read.csv(file.choose())
+
 
 #Cuarto punto####
