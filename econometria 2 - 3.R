30fa1cfe6721424a0b461d7be1435f099723abaf
JuanitaCortes <jucortesar@unal.edu.co>
2020-07-01 03:06:04 -0500

    3:06

--- econometria 2 - 3.R
+++ econometria 2 - 3.R
@@ -107,6 +107,168 @@ fore1<-autoplot(forecast::forecast(arima3, level = c(95), h = 7))+
                               scale_y_continuous(limit = c(4.25, 4.75))
 print(fore1)
 
+
+# intentos ##########################
+
+# serie M2 #######
+
+M2SL <- read_excel("M2SL.xls")
+View(M2SL)
+M2<-ts(data.frame(M2SL)$M2SL, frequency = 12, start = 1960)
+autoplot(M2)
+autoplot(log(M2))
+autoplot(diff(log(M2)))
+autoplot(diff(M2))
+plot(stl(M2,s.window = "periodic"))
+
+#Hacer diagrama de caja por trimestre
+(ciclo=cycle(M2))     #Ver el ciclo de la serie
+boxplot(M2~ciclo)
+
+
+#Descomponer la serie (componente tendencial, estacional y aleatorio)
+descompos = stl(M2,s.window = "periodic")
+plot(descompos)
+
+
+#fac no decae rapidamente, indicios de tendencia 
+acf(M2)
+pacf(M2)
+Diff_log_m2<-diff(log(M2))
+
+
+
+# Vamos a aplicar el test de raíz unitaria Dickey-Fuller, el cual nos dice si 
+# una serie tiene raíz unitaria o no. La hipótesis nula es que la serie tiene al menos una 
+# raíz unitaria (no es estacionaria), mientras la hipótesis alternativa dice que es estacionaria. 
+
+DF.Test = ur.df(Diff_log_m2, type="none", selectlags = "AIC")
+summary(DF.Test) #Rechazo la hipótesis nula, asi que Diff_log_m2 es estacionaria. 
+
+# fac y facp de la serie transformada 
+grid.arrange(
+  ggAcf(Diff_log_m2,lag.max=20,plot=T,lwd=2,xlab='',main='ACF del IPC'),
+  ggPacf(Diff_log_m2,lag.max=30,plot=T,lwd=2,xlab='',main='PACF del IPC')
+)
+
+#doble diferencia
+grid.arrange(
+  ggAcf(diff(Diff_log_m2),lag.max=20,plot=T,lwd=2,xlab='',main='ACF del IPC'),
+  ggPacf(diff(Diff_log_m2),lag.max=30,plot=T,lwd=2,xlab='',main='PACF del IPC')
+)
+
+autoplot(diff(diff(M2)))
+autoplot(Diff_log_m2)
+
+mar <- 4
+mma <- 4
+results <- c("p","q","AIC","SBC")
+for (i in 0:mar) {
+  for (j in 0:mma)  {
+    fitp <- arima(Diff_log_m2, order = c(i, 0, j), include.mean = TRUE)
+    results <- rbind(results,as.numeric(c(i, j, AIC(fitp), BIC(fitp)))) 
+  }
+}
+
+results
+# por criterios de info ####
+#ARIMA (1,0,3) de serie lm
+mod1<-arima(x = Diff_log_m2,order = c(1,0,3))
+
+#ARIMA (2,1,4) de serie lm
+mod2<-arima(x = Diff_log_m2,order = c(1,0,4))
+
+#ARIMA (4,1,2) de serie lm
+mod3<-arima(x = Diff_log_m2,order = c(3,0,4))
+
+#ARIMA (4,1,4) de serie lm
+mod4<-arima(x = Diff_log_m2,order = c(1,0,2),include.mean = TRUE)
+
+#ARIMA (4,1,4) de serie lm
+mod5<-arima(x = Diff_log_m2,order = c(3,0,2))
+
+library(lmtest)
+coeftest(mod2)  
+#significativos mod 2,4,5
+
+
+# Validacion del modelo
+layout(matrix(1:2, ncol = 2, nrow = 1))
+plot(mod5$resid)
+acf(mod5$resid, ylim=c(-1,1), main = "FAC de Residuales")
+
+for (i in 1:24) {print( Box.test(resid(mod5), lag=i,  type="Ljung") )}
+
+normalTest(mod5$resid, method="jb")
+jarque.bera.test(mod5$residuals)
+hist(mod5$residuals)
+plot(mod5$residuals)
+
+
+# parentesis ####
+which.max(mod4$resid)
+da <- rep(0,length(Diff_log_m2))
+da
+da[735] <- 1
+da
+Modelexp1 <- arima(Diff_log_m2, order =c(1,0,2), xreg = da)
+for (i in 1:24) {print( Box.test(resid(Modelexp1), lag=i,  type="Ljung") )}
+
+
+#PRONOSTICO####
+pron=predict(mod5,n.ahead = 7)
+#Pronosticar 5 a?os futuros
+v.futuros=pron$pred #predicciones en log
+pred<-exp(v.futuros)
+
+pred=forecast(mod5,h=20)
+library(ggplot2)
+autoplot(pred)
+
+#REAL vs AJUSTADO
+ts.plot(Diff_log_m2,(pred$fitted)[5,10],col=c("black","red"))
+
+autoplot(forecast(auto.arima(Diff_log_m2),h = 12))
+
+
+Des_ger
+names(Des_ger)
+
+# PIB Noruega / Ahora es desempleo alemania gg ####
+Pib_nor<- ts(data.frame(Des_ger)$LMUNRRTTDEM156S, frequency =12 , start = 1969)
+Pib_nor
+autoplot(Pib_nor)
+autoplot(log(Pib_nor))
+autoplot(diff(log(Pib_nor)))
+autoplot(diff(Pib_nor))
+plot(stl(Pib_nor,s.window = "periodic"))
+
+#Hacer diagrama de caja por trimestre
+(ciclo=cycle(Pib_nor))     #Ver el ciclo de la serie
+boxplot(Pib_nor~ciclo)
+
+acf(Pib_nor)
+pacf(Pib_nor)
+Diff_pib<-diff((Pib_nor))
+
+acf(diff(Pib_nor))
+pacf(diff(Pib_nor))
+# Vamos a aplicar el test de raíz unitaria Dickey-Fuller, el cual nos dice si 
+# una serie tiene raíz unitaria o no. La hipótesis nula es que la serie tiene al menos una 
+# raíz unitaria (no es estacionaria), mientras la hipótesis alternativa dice que es estacionaria. 
+
+DF.Test = ur.df(Diff_pib, type="none", selectlags = "AIC")
+summary(DF.Test) #Rechazo la hipótesis nula, asi que Diff_log_m2 es estacionaria. 
+
+# fac y facp de la serie transformada 
+grid.arrange(
+  ggAcf(Diff_pib,lag.max=30,plot=T,lwd=2,xlab='',main='ACF del IPC'),
+  ggPacf(Diff_pib,lag.max=30,plot=T,lwd=2,xlab='',main='PACF del IPC')
+)
+
+
+
+
 #Segundo punto####
 Data_UR<-read.csv(file.choose())
 View(Data_UR)
