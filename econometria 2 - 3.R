3cdaaff967b288bf50c912ac14bb261d0d026898
Raul Torres <rtorresj@unal.edu.co>
2020-07-03 20:20:22 -0500

    8:20

--- econometria 2 - 3.R
+++ econometria 2 - 3.R
@@ -1,7 +1,7 @@
 #Taller 3 - Econmetría 2####
 #Raul Torres, Juanita Cortes, David Orozco
 library(readr); library(urca); library(tseries); library(gridExtra); library(ggfortify); library(sandwich)
-library(forecast); library(seasonal); library(aTSA); library(readxl); library(timeDate); library(FinTS)
+library(forecast); library(seasonal); library(aTSA); library(readxl); library(timeDate); library(FinTS); library(dynlm)
 ############################################################################
 # This R function helps to interpret the output of the urca::ur.df function.
 interp_urdf <- function(urdf, level="5pct") {
@@ -138,7 +138,7 @@ interp_urdf <- function(urdf, level="5pct") {
 ############################################################################
 #Primer punto####
 Data_1<-read_delim(file.choose(),";", escape_double = FALSE, trim_ws = TRUE)
-IPC_DE<-ts(data.frame(Data_1)$IPC_DE, frequency = 12, start = 1960)
+IPC_DE<-ts(data.frame(Data_1)$IPC_DE[481:length(IPC_DE)], frequency = 12, start = 2000)
 
 summary(IPC_DE); kurtosis(IPC_DE)
 start(IPC_DE); end(IPC_DE)
@@ -148,8 +148,12 @@ monthplot(IPC_DE, col = "midnightblue")
 
 # Autocorrelación simple y parcial serie original
 grid.arrange(
-  ggAcf(IPC_DE,lag.max=30,plot=T,lwd=2,xlab='',main='ACF del IPC'),
-  ggPacf(IPC_DE,lag.max=30,plot=T,lwd=2,xlab='',main='PACF del IPC')
+  ggAcf(IPC_DE,lag.max=64,plot=T,lwd=2,xlab='',main='ACF del IPC', ylim=c(-1,1)),
+  ggPacf(IPC_DE,lag.max=64,plot=T,lwd=2,xlab='',main='PACF del IPC', ylim=c(-1,1)),
+  ggAcf(diff(IPC_DE),lag.max=64,plot=T,lwd=2,xlab='',main='ACF del difIPC', ylim=c(-1,1)),
+  ggPacf(diff(IPC_DE),lag.max=64,plot=T,lwd=2,xlab='',main='PACF del difIPC', ylim=c(-1,1)),
+  ggAcf(Diff_s,lag.max=64,plot=T,lwd=2,xlab='',main='ACF del IPC diferenciado estacional', ylim=c(-1,1)),
+  ggPacf(Diff_s,lag.max=64,plot=T,lwd=2,xlab='',main='PACF del IPC diferenciado estacional', ylim=c(-1,1))
 )
 
 # Gráfico serie en primera diferencia y diferencia de los log
@@ -162,20 +166,29 @@ monthplot(diff(log(IPC_DE)), col = "midnightblue")
 # Gráfico de autocorrelación simple y parcial diff log
 #claro componente estacional cada 12 periodo
 grid.arrange(
-  ggAcf(diff(log(IPC_DE)),lag.max=64,plot=T,lwd=2,xlab='',main='ACF del IPC'),
-  ggPacf(diff(log(IPC_DE)),lag.max=64,plot=T,lwd=2,xlab='',main='PACF del IPC')
+  ggAcf(diff(IPC_DE),lag.max=64,plot=T,lwd=2,xlab='',main='ACF del difIPC', ylim=c(-1,1)),
+  ggPacf(diff(IPC_DE),lag.max=64,plot=T,lwd=2,xlab='',main='PACF del difIPC', ylim=c(-1,1)),
+  ggAcf(Diff_log,lag.max=64,plot=T,lwd=2,xlab='',main='ACF del IPC diferenciado log', ylim=c(-1,1)),
+  ggPacf(Diff_log,lag.max=64,plot=T,lwd=2,xlab='',main='PACF del IPC diferenciado log', ylim=c(-1,1))
+  
 )
 
 #Pruebas de raiz unitaria
 #El tau me dice si la serie tiene o no al menos una raíz unitaria. 
 #El phi3 me dice si la tendecia es significativa
 #El phi2 me indica si la deriva es significativa
-summary(ur.df(IPC_DE,type = 'trend'));interp_urdf(ur.df(IPC_DE,type = 'trend'),level = "5pct")
-summary(ur.df(IPC_DE,type = 'drift'));interp_urdf(ur.df(IPC_DE,type = 'drift'),level = "5pct")
+summary(ur.df(IPC_DE,type = 'trend', selectlags = 'AIC'));interp_urdf(ur.df(IPC_DE,type = 'trend'),level = "5pct")
+summary(ur.df(IPC_DE,type = 'drift', selectlags = 'AIC'));interp_urdf(ur.df(IPC_DE,type = 'drift'),level = "5pct")
 #summary(ur.df(IPC_DE,type = 'none'));interp_urdf(ur.df(IPC_DE,type = 'none'),level = "5pct")
 
 #Transformaciones
+Diff<-diff(IPC_DE)
 Diff_log<- diff(log(IPC_DE))
+
+summary(ur.df(Diff,type = 'trend', selectlags = 'AIC'));interp_urdf(ur.df(Diff,type = 'trend'),level = "5pct")
+summary(ur.df(Diff,type = 'drift', selectlags = 'AIC'));interp_urdf(ur.df(Diff,type = 'drift'),level = "5pct")
+#summary(ur.df(Diff,type = 'none', selectlags = 'AIC'));interp_urdf(ur.df(Diff,type = 'none'),level = "5pct")
+
 
 #Diferencia estacional de la serie anterior 
 Diff_s<-diff(Diff_log,lag = 12,differences = 1)
@@ -188,57 +201,89 @@ grid.arrange(
   ggPacf(Diff_s,lag.max=64,plot=T,lwd=2,xlab='',main='PACF del IPC diferenciado estacional')
 )
 
+summary(ur.df(Diff_s,type = 'trend', selectlags = 'AIC'));interp_urdf(ur.df(Diff_s,type = 'trend'),level = "5pct")
+summary(ur.df(Diff_s,type = 'drift', selectlags = 'AIC'));interp_urdf(ur.df(Diff_s,type = 'drift'),level = "5pct")
+summary(ur.df(Diff_s,type = 'none', selectlags = 'AIC'));interp_urdf(ur.df(Diff_s,type = 'none'),level = "5pct")
+
 IPC_x11<-seas(diff(log(IPC_DE)),transform.function="none",x11="")
 plot(IPC_x11)
 IPC_SeasAdj<-seasadj(IPC_x11)
 autoplot(IPC_SeasAdj)
 autoplot(stl(diff(log(IPC_DE)), s.window = 'periodic')$time.series[,2])
 
-arima<-auto.arima(Diff_s)
-arima1<-arima(diff(log(IPC_DE)),order=c(1,0,0),seasonal=list(order=c(0,1,1),period=12))
-arima2<-arima(diff(log(IPC_DE)),order=c(0,0,1),seasonal=list(order=c(0,1,1),period=12))
-arima3<-arima(log(IPC_DE),order=c(2,2,3),seasonal=list(order=c(0,0,2),period=12))
-arima4<-arima(diff(log(IPC_DE)),order=c(0,0,2),seasonal=list(order=c(0,1,1),period=12))
+arima<-auto.arima(Diff)
+arima1<-arima(diff(log(IPC_DE)),order=c(1,0,0),seasonal=list(order=c(0,1,1),period=12), include.mean=T)
+arima2<-arima(diff(log(IPC_DE)),order=c(0,0,1),seasonal=list(order=c(0,1,1),period=12), include.mean=T)
+
+arima3<-Arima(Diff,order=c(1,0,1),seasonal=list(order=c(1,0,1),period=12), include.drift=T)
+
+arima4<-arima(diff(log(IPC_DE)),order=c(0,0,2),seasonal=list(order=c(0,1,1),period=12), include.mean=T)
 
 AR=3; MA=3
 arma_seleccion = function(AR.m, MA.m){
   for (i in 0:AR) {
     for (j in 0:MA)  {
-      fitp <- arima(diff(log(IPC_DE)), order = c(i, 0, j), seasonal=list(order=c(0,1,2),period=12))
+      fitp <- arima(Diff, order = c(i, 0, j), seasonal=list(order=c(1,0,1),period=12))
       print( c(i, j, AIC(fitp), BIC(fitp)) )
     }
   }
 }
-
 arma_seleccion(AR.m, MA.m)
 
-autoplot(cbind(log(IPC_DE), fitted(arima3)))
+autoplot(cbind(Diff, fitted(arima3))) + scale_x_continuous(limit = c(2000, 2025))
 
 hist(residuals(arima3))
 qqnorm(residuals(arima3))
 qqline(residuals(arima3))
-jarque.bera.test(residuals(arima3))
 shapiro.test(residuals(arima3))
 
 grid.arrange(
-  ggAcf(residuals(arima3),lag.max=30,plot=T,lwd=2,xlab='',main='ACF del IPC'),
-  ggPacf(residuals(arima3),lag.max=30,plot=T,lwd=2,xlab='',main='PACF del IPC')
+  ggAcf(residuals(arima3),lag.max=60,plot=T,lwd=2,xlab='',main='ACF de residuos SARIMA'),
+  ggPacf(residuals(arima3),lag.max=60,plot=T,lwd=2,xlab='',main='PACF de residuos SARIMA')
 )
 
-summary(ur.df(residuals(arima3),type = 'trend'))
-summary(ur.df(residuals(arima3),type = 'drift'))
-summary(ur.df(residuals(arima3),type = 'none'));interp_urdf(ur.df(residuals(arima3),type = 'none'),level = "5pct")
+summary(ur.df(residuals(arima3),type = 'trend', selectlags = 'AIC'))
+summary(ur.df(residuals(arima3),type = 'drift', selectlags = 'AIC'))
+summary(ur.df(residuals(arima3),type = 'none', selectlags = 'AIC'));interp_urdf(ur.df(residuals(arima3),type = 'none'),level = "5pct")
 
 Box.test(residuals(arima3),type='Ljung-Box',lag=length(IPC_DE)/4)
 Box.test(residuals(arima3),type='Box-Pierce',lag=length(IPC_DE)/4)
-ggtsdiag(arima3)
-arch <-arch.test(arima3, output=TRUE)
+checkresiduals(arima3, test = "BG")
+ArchTest(residuals(arima3), lags = length(IPC_DE)/4)
 
 fore1<-autoplot(forecast::forecast(arima3, level = c(95), h = 7))+
-  scale_x_continuous(limit = c(2010, 2025))+
-  scale_y_continuous(limit = c(4.5, 4.7))
+  scale_x_continuous(limit = c(2010, 2025))#+
+  #scale_y_continuous(limit = c(4.5, 4.7))
 print(fore1)
 
+library(tsoutliers)
+library(expsmooth)
+library(fma)
+
+## Identify Outliers
+
+outlier.1 <- tsoutliers::tso(Diff,types = c("AO","LS","TC"),maxit.iloop=10, tsmethod = "arima", args.tsmethod = list(order = c(1, 0, 1), seasonal = list(order = c(1, 0, 1))))
+outlier.1
+plot(outlier.1)
+
+n <- length(Diff)
+
+## Create Outliers Regressors for ARIMAX
+## Two type of outliers Level Shift (LS) and Temprory Change (TC)
+
+mo.ls <- outliers("LS", 12)
+ls <- outliers.effects(mo.ls, n)
+
+mo.tc <- outliers("TC", 20)
+tc <- outliers.effects(mo.tc, n)
+
+xreg.outliers <- cbind(ls,tc)
+
+
+## Create Arimax using Outliers as regressor variables.
+
+arima.model <- Arima(Diff,order=c(1,0,1),seasonal=list(order=c(1,0,1),period=12), include.drift=T,xreg=xreg.outliers)
+arima.model
 
 # intentos ##########################
 
@@ -265,8 +310,8 @@ autoplot(descompos)
 
 #fac no decae rapidamente, indicios de tendencia 
 grid.arrange(
-  ggAcf(M2,lag.max=30,plot=T,lwd=2,xlab='',main='ACF del M2'),
-  ggPacf(M2,lag.max=30,plot=T,lwd=2,xlab='',main='PACF del M2')
+  ggAcf(M2,lag.max=60,plot=T,lwd=2,xlab='',main='ACF del M2'),
+  ggPacf(M2,lag.max=60,plot=T,lwd=2,xlab='',main='PACF del M2')
 )
 
 # Vamos a aplicar el test de raíz unitaria Dickey-Fuller, el cual nos dice si 
@@ -285,15 +330,18 @@ grid.arrange(
 
 # fac y facp de la serie transformada 
 grid.arrange(
-  ggAcf(Diff_log_m2,lag.max=30,plot=T,lwd=2,xlab='',main='ACF del M2'),
-  ggPacf(Diff_log_m2,lag.max=30,plot=T,lwd=2,xlab='',main='PACF del M2')
+  ggAcf(Diff_log_m2,lag.max=60,plot=T,lwd=2,xlab='',main='ACF del dif logM2', ylim=c(-1,1)),
+  ggPacf(Diff_log_m2,lag.max=60,plot=T,lwd=2,xlab='',main='PACF del dif logM2', ylim=c(-1,1)),
+  ggAcf(diff(M2),lag.max=60,plot=T,lwd=2,xlab='',main='ACF del difM2', ylim=c(-1,1)),
+  ggPacf(diff(M2),lag.max=60,plot=T,lwd=2,xlab='',main='PACF del difM2', ylim=c(-1,1))
 )
+
 summary(ur.df(Diff_log_m2,type = 'trend'));interp_urdf(ur.df(Diff_log_m2,type = 'trend'),level = "5pct")
 summary(ur.df(Diff_log_m2,type = 'drift'));interp_urdf(ur.df(Diff_log_m2,type = 'drift'),level = "5pct")
 summary(ur.df(Diff_log_m2,type = 'none'));interp_urdf(ur.df(Diff_log_m2,type = 'none'),level = "5pct")
 
-mar <- 4
-mma <- 4
+mar <- 3
+mma <- 3
 results <- c("p","q","AIC","SBC")
 for (i in 0:mar) {
   for (j in 0:mma)  {
@@ -314,7 +362,7 @@ mod2<-arima(x = Diff_log_m2,order = c(1,0,4))
 mod3<-arima(x = Diff_log_m2,order = c(3,0,4))
 
 #ARIMA (4,1,4) de serie lm
-mod4<-Arima(y = Diff_log_m2,order = c(3,0,2), xreg = cbind(da, db))
+mod4<-Arima(y = Diff_log_m2,order = c(1,0,1), xreg = cbind(da))
 
 #ARIMA (4,1,4) de serie lm
 mod5<-arima(x = Diff_log_m2,order = c(3,0,2))
@@ -376,14 +424,11 @@ for (i in 1:24) {print( Box.test(resid(Modelexp1), lag=i,  type="Ljung") )}
 
 
 #PRONOSTICO####
-pron=predict(mod5,n.ahead = 7)
-#Pronosticar 5 a?os futuros
-v.futuros=pron$pred #predicciones en log
-pred<-exp(v.futuros)
+fore<-autoplot(forecast::forecast(fitted(mod4), level = c(95), h = 7))+
+  scale_x_continuous(limit = c(2010, 2025))#+
+#scale_y_continuous(limit = c(4.5, 4.7))
+print(fore)
 
-pred=forecast(mod5,h=20)
-library(ggplot2)
-autoplot(pred)
 
 #REAL vs AJUSTADO
 ts.plot(Diff_log_m2,(pred$fitted)[5,10],col=c("black","red"))
@@ -947,8 +992,8 @@ vecm_seleccion2 = function(max.lagind, max.lagdep){
 }
 vecm_seleccion2(max.lagind=4, max.lagdep=4)#El mejor es con 1 y 3 rezagos en independiente y dependiente
 
-VECM_451 <- dynlm(d(x4) ~  L(x4-beta_45*x5) + L(d(x4), 1) + L(d(x5),1)) ;summary(VECM_451) #El par?metro de velocidad de ajuste alpha tiene el signo esperado
-VECM_452 <- dynlm(d(x5) ~  L(x4-beta_45*x5)+ L(d(x5), 1:3) + L(d(x4),1)) ;summary(VECM_452) #El par?metro de velocidad de ajuste alpha tiene el signo esperado
+VECM_451 <- dynlm(d(x4) ~  L(x4-beta_45*x5) + L(d(x4), 1:4) + L(d(x5),1:2)) ;summary(VECM_451)
+VECM_452 <- dynlm(d(x5) ~  L(x4-beta_45*x5)+ L(d(x5), 1:3) + L(d(x4),1)) ;summary(VECM_452) 
 
 # Usando errores estandar robustos
 coeftest(VECM_451, vcov. = vcovHC(VECM_451))
