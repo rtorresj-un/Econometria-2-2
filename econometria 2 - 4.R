b6d37ea0e51fe20ea3d5668d67cab5109c212cf8
JuanitaCortes <jucortesar@unal.edu.co>
2020-07-20 16:59:34 -0500

    5:00

--- econometria 2 - 4.R
+++ econometria 2 - 4.R
@@ -155,6 +155,140 @@ irf_ggplot<-function(VAR, impulso, respuesta){
 ############################################################################
 
 ####Punto 1####
+###############################
+VAR INFLACION DESEMPLEO ALEMANIA 1991- 2020 
+353 OBSERVACIONES 1991-01 ; 2020-05
+###############################
+
+#Harmonised Index of Consumer Prices (HICP) #####
+ipc_ajustado <- read_csv("C:/Users/USUARIO/Downloads/ipc_ajustado.csv", 
+                         +     col_names = FALSE)
+attach(ipc_ajustado)
+Date1<-as.Date(Date, format = '%d/%m/%Y')
+IPC_DE<-ts(data.frame(ipc_ajustado)$X2[(1:353)], frequency = 12, start = 1991)
+View(IPC_DE)
+
+#Descripción de IPC
+summary(IPC_DE); kurtosis(IPC_DE)
+start(IPC_DE); end(IPC_DE)
+#Clara tendencia de la serie
+autoplot(IPC_DE,main = "IPC Alemania (enero 1991 - abril 2020)")
+monthplot(IPC_DE, col = "midnightblue")
+#Serie estacional
+descompos = stl(IPC_DE,s.window = "periodic"); autoplot(descompos)
+
+# Autocorrelación simple y parcial serie original.
+grid.arrange(
+        ggAcf(IPC_DE,lag.max=60,plot=T,lwd=2,xlab='',main='ACF del IPC', ylim=c(-1,1)),
+        ggPacf(IPC_DE,lag.max=60,plot=T,lwd=2,xlab='',main='PACF del IPC', ylim=c(-1,1)),
+        ggAcf(diff(IPC_DE),lag.max=60,plot=T,lwd=2,xlab='',main='ACF del IPC diferenciado', ylim=c(-1,1)),
+        ggPacf(diff(IPC_DE),lag.max=60,plot=T,lwd=2,xlab='',main='PACF del IPC diferenciado', ylim=c(-1,1))
+)
+#Es necesario diferenciar la serie.
+Diff_ipc<- diff(IPC_DE)
+grid.arrange(
+        autoplot(Diff_ipc, col = "midnightblue")
+)  
+monthplot(Diff, col = "midnightblue")
+
+# Gráfico de autocorrelación simple y parcial diff 
+grid.arrange(
+        ggAcf(Diff_ipc,lag.max=64,plot=T,lwd=2,xlab='',main='ACF del difIPC', ylim=c(-1,1)),
+        ggPacf(Diff_ipc,lag.max=64,plot=T,lwd=2,xlab='',main='PACF del difIPC', ylim=c(-1,1))
+)
+
+#Pruebas de raiz unitaria
+summary(ur.df(IPC_DE,type = 'trend', selectlags = 'AIC'));interp_urdf(ur.df(IPC_DE,type = 'trend'),level = "5pct")
+summary(ur.df(IPC_DE,type = 'drift', selectlags = 'AIC'));interp_urdf(ur.df(IPC_DE,type = 'drift'),level = "5pct") #caminata aleatoria con deriva
+summary(ur.df(IPC_DE,type = 'none'));interp_urdf(ur.df(IPC_DE,type = 'none'),level = "5pct")
+
+summary(ur.df(Diff_ipc,type = 'trend', selectlags = 'AIC'));interp_urdf(ur.df(Diff_ipc,type = 'trend'),level = "5pct")
+summary(ur.df(Diff_ipc,type = 'drift', selectlags = 'AIC'));interp_urdf(ur.df(Diff_ipc,type = 'drift'),level = "5pct") #caminata aleatoria con deriva
+summary(ur.df(Diff,type = 'none', selectlags = 'AIC'));interp_urdf(ur.df(Diff,type = 'none'),level = "5pct")
+
+# Serie tiene tendencia lineal, se realiza la prueba df con tendencia y no hay presencia de raiz unitaria 
+
+
+
+
+
+
+# Harmonized Unemployment Rate: Total: All Persons for Germany, Percent, Seasonally Adjusted ####
+Data_2<-read_excel(file.choose())
+attach(Data_2)
+D_DE<-ts(data.frame(Data_2)$desempleo, frequency = 12, start = 1991)
+
+
+#Descripción de IPC
+summary(D_DE); kurtosis(D_DE)
+start(D_DE); end(D_DE)
+
+autoplot(D_DE,main = "Desempleo Alemania (enero 1991 - abril 2020)")
+monthplot(D_DE, col = "midnightblue")
+
+#Serie estacional
+descompos = stl(D_DE,s.window = "periodic"); autoplot(descompos)
+
+# Autocorrelación simple y parcial serie original.
+grid.arrange(
+        ggAcf(D_DE,lag.max=60,plot=T,lwd=2,xlab='',main='ACF del Desempleo', ylim=c(-1,1)),
+        ggPacf(D_DE,lag.max=60,plot=T,lwd=2,xlab='',main='PACF del Desempleo', ylim=c(-1,1)),
+        ggAcf(diff(D_DE),lag.max=60,plot=T,lwd=2,xlab='',main='ACF del Desempleo diferenciado', ylim=c(-1,1)),
+        ggPacf(diff(D_DE),lag.max=60,plot=T,lwd=2,xlab='',main='PACF del Desempleo diferenciado', ylim=c(-1,1))
+)
+#Es necesario diferenciar la serie.
+#Transformaciones.
+Diff_DE<-diff(D_DE)
+
+
+# Gráfico serie en primera diferencia y diferencia de los log
+#Al aplicar log se logra estabilizar la varianza
+grid.arrange(
+        autoplot(Diff_DE, col = "midnightblue")
+)  
+monthplot(Diff_DE, col = "midnightblue")
+
+# Gráfico de autocorrelación simple y parcial diff log
+#claro componente estacional cada 12 periodo
+grid.arrange(
+        ggAcf(Diff_DE,lag.max=64,plot=T,lwd=2,xlab='',main='ACF dif Desempleo', ylim=c(-1,1)),
+        ggPacf(Diff_DE,lag.max=64,plot=T,lwd=2,xlab='',main='PACF dif Desempleo', ylim=c(-1,1)),
+        ggAcf(Diff_log_DE,lag.max=64,plot=T,lwd=2,xlab='',main='ACF del Desempleo diferenciado log', ylim=c(-1,1)), #el logaritmo no aporta mejoría a la autocorrelación de la serie
+        ggPacf(Diff_log_DE,lag.max=64,plot=T,lwd=2,xlab='',main='PACF del Desempleo diferenciado log', ylim=c(-1,1)),
+        ggAcf(Diff_s_DE,lag.max=64,plot=T,lwd=2,xlab='',main='ACF del Desempleo diferenciado estacional', ylim=c(-1,1)), #diferenciar la serie mejora la autocorrelación de la serie pero puede no ser necesario 
+        ggPacf(Diff_s_DE,lag.max=64,plot=T,lwd=2,xlab='',main='PACF del Desempleo diferenciado estacional', ylim=c(-1,1)) #porque el efecto persiste, puede corregirse con un SARIMA(1,0,1)(1,0,1)[12]
+)
+
+#Pruebas de raiz unitaria
+summary(ur.df(D_DE,type = 'trend', selectlags = 'AIC'));interp_urdf(ur.df(D_DE,type = 'trend'),level = "5pct")
+summary(ur.df(D_DE,type = 'drift', selectlags = 'AIC'));interp_urdf(ur.df(D_DE,type = 'drift'),level = "5pct") 
+summary(ur.df(D_DE,type = 'none'));interp_urdf(ur.df(D_DE,type = 'none'),level = "5pct") #caminata aleatoria 
+
+summary(ur.df(Diff_DE,type = 'trend', selectlags = 'AIC'));interp_urdf(ur.df(Diff_DE,type = 'trend'),level = "5pct")
+summary(ur.df(Diff_DE,type = 'drift', selectlags = 'AIC'));interp_urdf(ur.df(Diff,type = 'drift'),level = "5pct")
+summary(ur.df(Diff_DE,type = 'none', selectlags = 'AIC'));interp_urdf(ur.df(Diff,type = 'none'),level = "5pct")
+
+plot(Diff_DE); mean(Diff)
+
+
+GER<-cbind(Diff_DE, Diff_ipc)
+VARselect(GER, lag.max=40,type = "none", season = NULL) # estimar var 2 o 7 
+VARselect(GER, lag.max=20,type = "const", season = NULL)# estimar var 3 o 2
+
+#VAR con sólo intercepto.
+V.dr.1= VAR(GER, p=2, type="const", season=NULL) 
+summary(V.dr.1) #El intercepto es significativo en una ecuación.
+
+#VAR sin términos determinísticos.
+V.no.1 = VAR(GER, p=2, type="none", season=NULL)  
+summary(V.no.1)
+roots(V.dr.1)
+roots(V.no.1)
+Acoef(V.dr.1)
+
+
+
+
 
 ####Punto 2####
 Datos<-read.csv(file.choose())
